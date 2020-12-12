107ff6399ea5adbd1c7e2580b5aeb95ca2dee491
EldarionDev <janek.schultes.privat@gmail.com>
2020-12-12 11:59:19 +0100

    Improvements to the code base

--- src/engine/graphics.rs
+++ src/engine/graphics.rs
@@ -7,7 +7,7 @@ pub struct Graphic {
 impl Graphic {
     pub fn new(glfw_instance: &mut glfw::Glfw) -> Result<Self, glfw::Error> {
         Ok(Graphic {
-            game_window: game_window::GameWindow::new(1920.0, 1080.0, "Fourth Age", glfw_instance)?,
+            game_window: game_window::GameWindow::new(1920, 1080, "Fourth Age", glfw_instance)?,
         })
     }
 }
--- src/engine/graphics/game_window.rs
+++ src/engine/graphics/game_window.rs
@@ -1,24 +1,22 @@
 pub struct GameWindow {
-    resolution: (f32, f32),
+    resolution: (u32, u32),
     title: String,
     glfw_window: glfw::Window,
 }
 
 impl GameWindow {
-    pub fn new(resX: f32, resY: f32, window_title: &str, glfw_instance: &mut glfw::Glfw) -> Result<Self, glfw::Error> {
-        let tmp: String =  window_title.to_string();
-        
+    pub fn new(res_y: u32, res_x: u32, window_title: &str, glfw_instance: &mut glfw::Glfw) -> Result<Self, glfw::Error> {
         glfw_instance.window_hint(glfw::WindowHint::ContextVersion(3, 3));
         glfw_instance.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-        let (mut window, _) = glfw_instance.create_window(1920, 1080, "The Fourth Age", glfw::WindowMode::Windowed)
+        let (mut window, _) = glfw_instance.create_window(res_x, res_y, window_title, glfw::WindowMode::Windowed)
             .expect("Failed to create GLFW window");
         window.set_key_polling(true);
         window.set_framebuffer_size_polling(true);
         gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
         
         Ok(GameWindow {
-            resolution: (resX, resY),
-            title: String::clone(&tmp),
+            resolution: (res_x, res_y),
+            title: window_title.to_string(),
             glfw_window: window,
         })
     }
