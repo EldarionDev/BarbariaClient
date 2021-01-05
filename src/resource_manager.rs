38b75488880187564fcce239255df87665280eb5
EldarionDev <janek.schultes.privat@gmail.com>
2021-01-05 13:49:43 +0100

    Started implementing resource manager

--- src/resource_manager.rs
+++ src/resource_manager.rs
@@ -1,3 +1,6 @@
+use std::{fs, path::PathBuf};
+use std::io;
+
 pub struct ResourceManager {
     asset_path: String,
     config_path: String,
@@ -13,5 +16,33 @@ impl ResourceManager {
 
     pub fn set_world(world_path: &str) {
 
+    }
+
+    pub fn get_shaders(self) -> Vec<String>{
+        let shader_path =  self.asset_path.to_string() + "/shaders/";
+
+        return self.get_dir_files(&shader_path);
+    }
+
+    fn get_dir_files<'a>(self, path: &str) -> Vec<String>{
+        
+        let files = match fs::read_dir(path) {
+            Err(_) => panic!("Shader directory could not be opened!"),
+            Ok(i) => i
+        };
+
+        let mut file_names: Vec<String> = vec![];
+
+        for file in files {
+            file_names.push(file.unwrap().path().to_str().unwrap().to_string());
+        }
+
+        let mut return_value: Vec<String> = vec![];
+        for file_name in file_names {
+            let full_path = path.to_string() + &file_name;
+            return_value.push(full_path);
+        }
+
+        return return_value;
     }
 }
